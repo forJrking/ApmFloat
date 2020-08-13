82ae315dc657931c1749b3c00cedda64c3a0999a
forjrking <forjrking@sina.com>
2020-08-13 10:48:58 +0800

    handler

---
+++ components_tools/src/main/java/com/forjrking/tools/thread/WeakReferenceHandler.java
@@ -0,0 +1,74 @@
+package com.forjrking.tools.thread;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Convenience class for making a static inner {@link Handler} class that keeps
+ * a {@link WeakReference} to its parent class. If the reference is cleared, the
+ * {@link WeakReferenceHandler} will stop handling {@link Message}s.
+ * <p>
+ * Example usage:
+ * <pre>
+ * private final MyHandler mHandler = new MyHandler(this);
+ *
+ * private static class MyHandler extends WeakReferenceHandler<MyClass> {
+ *     protected void handleMessage(Message msg, MyClass parent) {
+ *         parent.onMessageReceived(msg.what, msg.arg1);
+ *     }
+ * }
+ * </pre>
+ * </p>
+ * 主要用于防止内存泄漏问题 需要实现类必须为static
+ * @param <T> The handler's parent class.
+ */
+public abstract class WeakReferenceHandler<T> extends Handler {
+    private final WeakReference<T> mParentRef;
+
+    /**
+     * Constructs a new {@link WeakReferenceHandler} with a reference to its
+     * parent class.
+     *
+     * @param parent The handler's parent class.
+     */
+    public WeakReferenceHandler(T parent) {
+        mParentRef = new WeakReference<T>(parent);
+    }
+
+    /**
+     * Constructs a new {@link WeakReferenceHandler} with a reference to its
+     * parent class.
+     *
+     * @param parent The handler's parent class.
+     * @param looper The looper.
+     */
+    public WeakReferenceHandler(T parent, Looper looper) {
+        super(looper);
+        mParentRef = new WeakReference<T>(parent);
+    }
+
+    @Override
+    public final void handleMessage(Message msg) {
+        final T parent = getParent();
+        if (parent == null) {
+            return;
+        }
+        handleMessage(msg, parent);
+    }
+
+    /**
+     * @return The parent class, or {@code null} if the reference has been
+     *         cleared.
+     */
+    protected T getParent() {
+        return mParentRef.get();
+    }
+
+    /**
+     * Subclasses must implement this to receive messages.
+     */
+    protected abstract void handleMessage(Message msg, T parent);
+}
